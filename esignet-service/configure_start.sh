e40166b623886c9b6e2dc0a1eb7a4dab64b6bcb0
pramod belal <126258949+pramod-technoforte@users.noreply.github.com>
2023-09-04 10:55:40 +0530

    Update configure_start.sh

--- esignet-service/configure_start.sh
+++ esignet-service/configure_start.sh
@@ -44,7 +44,7 @@ else
 fi
 
 echo "Attempting to install"
-cd ./$DIR_NAME && chmod +x install.sh && sudo ./install.sh
+cd ./$DIR_NAME && chmod +x install.sh && sudo -S ./install.sh 
 echo "Installation complete"
 cd $work_dir
 
